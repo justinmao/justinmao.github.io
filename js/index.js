b436bf4deae29f2506137d7096d33bd1b5b1d533
Justin Mao <justinmao@live.com>
2016-12-12 03:40:36 -0500

    tacked on an image preloader to prevent frame skips

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
+$.preload = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    $("<img />").attr("src", arguments[i]);
+  }
+}
+$.preload("/resources/img2.jpg");
+
 $(document).ready(function() {
   $("#link-about").click(function() {
     $("#main").css("margin-top", "12px");
-    $("#banner").css("height", "100px").fadeOut(1200);
-    $("#banner-about").delay(1200).fadeIn(1200);
+    $("#banner").css("height", "100px").fadeOut(1000);
+    $("#banner-about").delay(1000).fadeIn(1000);
 
     $("#about").fadeIn(1000);
   });
