52df211ca6c6aaeb71613d3285e60044e83aea88
Justin Mao <justinmao@live.com>
2016-12-12 03:32:24 -0500

    fixed a visual scaling issue

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("#link-about").click(function() {
     $("#main").css("margin-top", "12px");
-    $("#banner").css("height", "100px").fadeOut(1000);
-    $("#banner-about").delay(1000).fadeIn(1000);
+    $("#banner").css("height", "100px").fadeOut(1200);
+    $("#banner-about").delay(1200).fadeIn(1200);
 
-    $("#about").fadeIn(950);
+    $("#about").fadeIn(1000);
   });
 });
