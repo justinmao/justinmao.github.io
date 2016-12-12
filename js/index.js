607219183cd4c0495c28ca2701385be3c8e8d9b6
Justin Mao <justinmao@live.com>
2016-12-12 03:57:48 -0500

    forgot a transition tag

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $.preload("/resources/img2.jpg");
 
 $(document).ready(function() {
   $("#link-about").click(function() {
-    $("#spacer").css("height", "0px");
+    $("#spacer").css("height", "12px");
     $("#banner").css("height", "100px").fadeOut(1000);
     $("#banner-about").delay(1000).fadeIn(1000);
 
